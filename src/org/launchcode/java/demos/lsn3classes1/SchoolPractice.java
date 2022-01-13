eead2538589cb26a550adb73b581d8a5a38eb255
Alexander Bourne <afb989@outlook.com>
2022-01-12 22:15:47 -0500

    chapter 4 exercises complete

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -2,6 +2,17 @@ package org.launchcode.java.demos.lsn3classes1;
 
 public class SchoolPractice {
     public static void main(String[] args) {
-        // Instantiate your Student class for part 2 here!
+//        var student1 = new Student();
+//        student1.setName("Rick Grimes");
+//        student1.setStudentId(8675309);
+//        student1.setNumberOfCredits(6);
+//        student1.setGpa(3.2);
+
+        var student1 = new Student("Negan", 12321, 9, 2.8);
+
+        System.out.println("Name: " + student1.getName());
+        System.out.println("Student ID: " + student1.getStudentId());
+        System.out.println("Credits: " + student1.getNumberOfCredits());
+        System.out.println("GPA: " + student1.getGpa());
     }
 }
