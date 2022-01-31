ecd326f9ad43b9d741b707d0f56f6bb8138491a9
Alex Bourne <afb989@outlook.com>
2022-01-30 21:02:21 -0500

    chapter 8 exercises complete

--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -1,6 +1,7 @@
 package org.launchcode.java.demos.lsn7interfaces;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args){
@@ -8,12 +9,21 @@ public class Main {
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
 
-        // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
-        //  field.
+        flavors.sort(new FlavorComparator());
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-        //  field.
+        cones.sort(new ConeComparator());
 
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+        System.out.println("What's your favorite flavor?\n");
+
+        for (int i = 0; i < flavors.size(); i++){
+            System.out.println(flavors.get(i).getName());
+        }
+
+        System.out.println("--------------------------");
+        System.out.println("Prices by cone selection:\n");
+
+        for (int i = 0; i < cones.size(); i++){
+            System.out.println(cones.get(i).getName() + " - $" + cones.get(i).getCost());
+        }
     }
 }
